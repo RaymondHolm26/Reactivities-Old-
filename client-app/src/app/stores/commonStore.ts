57267c0db08ee0f79029c73b0a15e9a236106102
RaymondHolm26 <raymond.holm5@gmail.com>
2021-04-20 11:02:12 +0200

    End of section 12 and 13

--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -1,14 +1,35 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, reaction } from "mobx";
 import { ServerError } from "../models/serverError";
 
 export default class CommonStore {
     error: ServerError | null = null;
+    token: string | null = window.localStorage.getItem('_RequestVerificationToken');
+    appLoaded = false;
 
     constructor() {
         makeAutoObservable(this);
+
+        reaction(
+            () => this.token,
+            token => {
+                if (token) {
+                    window.localStorage.setItem('_RequestVerificationToken', token);
+                } else {
+                    window.localStorage.removeItem('_RequestVerificationToken');
+                }
+            }
+        )
     }
 
     setServerError = (error: ServerError) => {
         this.error = error;
+    }
+
+    setToken = (token: string | null) => {
+        this.token = token;
+    }
+
+    setAppLoaded = () => {
+        this.appLoaded = true;
     }
 }
