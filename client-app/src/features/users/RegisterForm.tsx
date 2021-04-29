de3a77b5d7ca502ef1e79d3c317651d6e7b95120
RaymondHolm26 <raymond.holm5@gmail.com>
2021-04-29 19:42:44 +0200

    Middle of section 14

--- client-app/src/features/users/RegisterForm.tsx
+++ client-app/src/features/users/RegisterForm.tsx
@@ -29,7 +29,11 @@ export default observer(function RegisterForm() {
       })}
     >
       {({ handleSubmit, isSubmitting, errors, isValid, dirty }) => (
-        <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+        <Form
+          className="ui form error"
+          onSubmit={handleSubmit}
+          autoComplete="off"
+        >
           <Header
             as="h2"
             content="SIgn up to Reactivities"
