6a4dc2947efd78a53cae8549c8a11adae2bb4ba7
RaymondHolm26 <raymond.holm5@gmail.com>
2021-05-01 15:14:49 +0200

    End of section 17

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -5,55 +5,57 @@ import { User, UserFormValues } from "../models/user";
 import { store } from "./store";
 
 export default class UserStore {
-    user: User | null = null;
+  user: User | null = null;
 
-    constructor() {
-        makeAutoObservable(this)
-    }
+  constructor() {
+    makeAutoObservable(this);
+  }
 
-    get isLoggedIn() {
-        return !!this.user;
-    }
+  get isLoggedIn() {
+    return !!this.user;
+  }
 
-    login = async (credentials: UserFormValues) => {
-        try {
-            const user = await agent.Account.login(credentials);
-            store.commonStore.setToken(user.token);
-            runInAction(() => this.user = user);
-            history.push('/activities');
-            store.modalStore.closeModal();
-        }
-        catch (error) {
-            throw error;
-        }
+  login = async (credentials: UserFormValues) => {
+    try {
+      const user = await agent.Account.login(credentials);
+      store.commonStore.setToken(user.token);
+      runInAction(() => (this.user = user));
+      history.push("/activities");
+      store.modalStore.closeModal();
+    } catch (error) {
+      throw error;
     }
+  };
 
-    logout = () => {
-        store.commonStore.setToken(null);
-        window.localStorage.removeItem('_RequestVerificationToken');
-        this.user = null;
-        history.push('/');
-    }
+  logout = () => {
+    store.commonStore.setToken(null);
+    window.localStorage.removeItem("_RequestVerificationToken");
+    this.user = null;
+    history.push("/");
+  };
 
-    getUser = async () => {
-        try {
-            const user = await agent.Account.current();
-            runInAction(() => this.user = user);
-        } catch (error) {
-            console.log(error);
-        }
+  getUser = async () => {
+    try {
+      const user = await agent.Account.current();
+      runInAction(() => (this.user = user));
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    register = async (credentials: UserFormValues) => {
-        try {
-            const user = await agent.Account.register(credentials);
-            store.commonStore.setToken(user.token);
-            runInAction(() => this.user = user);
-            history.push('/activities');
-            store.modalStore.closeModal();
-        }
-        catch (error) {
-            throw error;
-        }
+  register = async (credentials: UserFormValues) => {
+    try {
+      const user = await agent.Account.register(credentials);
+      store.commonStore.setToken(user.token);
+      runInAction(() => (this.user = user));
+      history.push("/activities");
+      store.modalStore.closeModal();
+    } catch (error) {
+      throw error;
     }
+  };
+
+  setImage = (image: string) => {
+    if (this.user) this.user.image = image;
+  };
 }
