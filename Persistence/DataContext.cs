9de6539ddede6da226eea688a158e4997bcf905a
RaymondHolm26 <raymond.holm5@gmail.com>
2021-05-05 08:21:16 +0200

    End of section 19

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -13,6 +13,7 @@ namespace Persistence
         public DbSet<Activity> Activities { get; set; }
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Comment> Comments { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
@@ -22,6 +23,8 @@ namespace Persistence
             builder.Entity<ActivityAttendee>().HasOne(u => u.AppUser).WithMany(a => a.Activities).HasForeignKey(aa => aa.AppUserId);
             
             builder.Entity<ActivityAttendee>().HasOne(u => u.Activity).WithMany(a => a.Attendees).HasForeignKey(aa => aa.ActivityId);
+
+            builder.Entity<Comment>().HasOne(a => a.Activity).WithMany(c => c.Comments).OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
--- client-app/src/features/profiles/ProfileEditForm.tsx
+++ client-app/src/features/profiles/ProfileEditForm.tsx
@@ -1,6 +1,6 @@
 import { Form, Formik } from "formik";
 import { observer } from "mobx-react-lite";
-import React, { useState } from "react";
+import React from "react";
 import { useStore } from "../../app/stores/store";
 import * as Yup from "yup";
 import MyTextInput from "../../app/common/form/MyTextInput";
